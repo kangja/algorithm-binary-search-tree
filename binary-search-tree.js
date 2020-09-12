28bc51f581a0924b36ce30b3f61fe2886e2a36ad
hi <kangja1996@gmail.com>
2020-09-12 00:06:58 -0400

    searchTree function finished

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -24,6 +24,15 @@ class BST {
       this.root = new Node(data);
       return;
     } else {
+
+      // searchTree is a recursive function where we pass in node in the function, which starts as the root node. 
+      
+      // If the data we pass in is lower than node.data, that means we are going to put the node on the left side of the tree.If node.left is null or empty, we will assign node.left to new Node and then we return. 
+      
+      // But, if node.left is not null, meaning, if node.left is present, we keep searching. It will go back to the beginning of the searchTree function and find where to put the node. 
+
+      // If data is bigger than node.data and if node.right is null/empty, we assign node.right to new node and we can return. 
+      // If node.right does not equal null, we keep searching. Else, meaning, if the data is not less and greater than than node.data, meaning, if they are equal, we return null. 
       const searchTree = function (node) {
         if (data < node.data) {
           if (node.left === null) {
@@ -32,8 +41,19 @@ class BST {
           } else if (node.left != null) {
             return searchTree(node.left);
           }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
         }
-      }
+      };
+      return searchTree(node);
     }
   }
+  findMin() {}
 }
